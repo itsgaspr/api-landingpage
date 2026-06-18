f93cf33cdf3d2f9c87c097ef41cdab276f0314a8
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-06-19 00:20:48 +0200

    add Features section to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Hero from "./sections/Hero"
+import Features from "./sections/Features"
 
 const App = () => {
     return (
         <>
             <Header />
             <Hero />
+            <Features/>
         </>
     )
 }
