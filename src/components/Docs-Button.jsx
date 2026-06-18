c8ad1ed67717ee72719f0e7a0acf0fe5a569164e
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-06-19 00:13:02 +0200

    feat: add prop to button 'view docs'

--- src/components/Docs-Button.jsx
+++ src/components/Docs-Button.jsx
@@ -1,7 +1,7 @@
-export default function DocsCta() {
+export default function DocsCta({text}) {
     return (
-        <button className="docs-btn">
-            Read the docs <i class="fa-solid fa-arrow-right-long fa-fade"></i>
+        <button className="docs-btn" id="docs-btn">
+            {text} <i class="fa-solid fa-arrow-right-long fa-fade"></i>
         </button>
     )
 }
