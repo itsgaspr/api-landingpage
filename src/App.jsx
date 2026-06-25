42ae58e743ce2f5ef52c84a03e66b6312aeff81d
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-06-25 22:05:55 +0200

    feat: add usecases section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Hero from "./sections/Hero"
 import Features from "./sections/Features"
+import UseCases from "./sections/UseCases"
 
 const App = () => {
     return (
@@ -8,6 +9,8 @@ const App = () => {
             <Header />
             <Hero />
             <Features/>
+            <UseCases/>
+
         </>
     )
 }
