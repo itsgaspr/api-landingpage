4a6999ec54a10d4d3414bbabd69446e682d26a5c
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-06-28 18:17:09 +0200

    create sectionHeader component

---
+++ src/components/SectionHeader.jsx
@@ -0,0 +1,12 @@
+export default function SectionHeader({title, description}) {
+    return (
+        <div className="headers flex flex-col gap-4 w-full mb-12">
+            <h2 className="text-3xl font-normal text-[#3B82F6]">
+                {title}
+            </h2>
+            <p className="text-md leading-6 text-justify tracking-tight w-1/3">
+                {description}
+            </p>
+        </div>
+    )
+}
