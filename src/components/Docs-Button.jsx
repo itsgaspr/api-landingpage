b79463da437878a3f27d48bdf055bbf5e602322c
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-06-29 16:41:30 +0200

    feat: button click takes to docs

--- src/components/Docs-Button.jsx
+++ src/components/Docs-Button.jsx
@@ -1,6 +1,11 @@
 export default function DocsCta({text}) {
+
+    const redirectToDocs = () => {
+        window.open("https://moz-demog-api.vercel.app/v1/docs", "_blank", "noopener,noreferer");
+    }
+
     return (
-        <button className="docs-btn font-normal cursor-pointer text-[#3B82F6] capitalize">
+        <button className="docs-btn font-normal cursor-pointer text-[#3B82F6] capitalize" onClick={redirectToDocs}>
             {text} <i class="fa-solid fa-arrow-right-long fa-fade text-[#3B82F6] ml-3"></i>
         </button>
     )
